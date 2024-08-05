4acb42ed8e358450eddc3042f3d7fe50d759819f
0768800022 <huycuong140203@gmail.com>
2024-08-06 00:00:54 +0700

    uploaD

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+// HTTP logger
+app.use(morgan('combined'));
 
-app.get('/home', (req, res) => {
-    return res.send(`<h1 style="color:red;">Hello World!</h1>`)
+// Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
