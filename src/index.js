9c86a3a013381d84594f61e749ab9f4432dea94f
0768800022 <huycuong140203@gmail.com>
2024-08-07 15:23:59 +0700

    uploadd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({
@@ -23,6 +23,10 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.render('news');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search');
 })
 
 
