2e97756edcbdd83fc37be0f83e349265e5f74d61
0768800022 <huycuong140203@gmail.com>
2024-08-11 14:33:04 +0700

    Course detail page

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,20 +1,18 @@
 const Course = require('../models/Course')
-const { multipleMongooseToObject } = require('../../util/mongoose') 
+const { mongooseToObject } = require('../../util/mongoose') 
 
 
 class CourseController {
-
-    // show(req, res, next) {
-    //     Course.find({})
-    //         .then(courses => res.render('course', {
-    //             courses: multipleMongooseToObject(courses)
-    //         }))
-    //         .catch(next)
-    // }
-    show(req, res) {
-        res.send('COURSE DETAIL')
+    
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then(course => {
+                res.render('courses/show', {
+                    course: mongooseToObject(course)
+                });
+            })
+            .catch(next); // Fix the typo here
     }
-
 }
 
 module.exports = new CourseController;
--- src/routes/course.js
+++ src/routes/course.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
 
-
 router.get('/:slug', courseController.show);
 
 module.exports = router;
