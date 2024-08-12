3ec2956cee13f50d06353ed06270c8e707085699
0768800022 <huycuong140203@gmail.com>
2024-08-12 23:56:22 +0700

    Delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -49,7 +49,13 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
-    
+
+    // [DELETE] /course/:id
+    delete(req, res, next) {
+        Course.deleteOne({_id: req.params.id})
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController;
